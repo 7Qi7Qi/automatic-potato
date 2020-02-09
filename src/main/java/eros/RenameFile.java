0ec8692b1dfe7d428393aa32489a8f54adb37338
零塵 <1922779185@qq.com>
2020-02-09 19:17:04 +0800

    Hades

---
+++ src/main/java/eros/RenameFile.java
@@ -0,0 +1,24 @@
+package eros;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RenameFile {
+  final static String SUCCESS = "Rename File Success";
+  final static String FAILURE = "Fail to Rename";
+
+  public static void main(String[] args) {
+    System.out.println("Current Directory: "+ new File(".").getAbsolutePath());
+    String oldFilePath = "D:/11.txt";
+    String newFilePath = "D:/22.txt";
+    if (rename(oldFilePath, newFilePath))    System.out.println(SUCCESS+" File.");
+    else System.out.println(FAILURE+" File.");
+    String oldFolderPath = "D:/New Folder";
+    String newFolderPath = "D:/New Folder1";
+    if (rename(oldFolderPath, newFolderPath))    System.out.println(SUCCESS+" Folder.");
+    else System.out.println(FAILURE+" Folder.");
+  }
+  private static boolean rename(String s1, String s2){
+    return new File(s1).renameTo(new File(s2));
+  }
+}
